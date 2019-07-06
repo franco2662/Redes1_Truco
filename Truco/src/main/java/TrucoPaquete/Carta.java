63e2cf756c651a6636e23af7f64f3d1b93dc7089
Franco Marino <franco2662@gmail.com>
2019-07-06 01:56:16 -0400

    Archivos del proyecto

---
+++ Truco/src/main/java/TrucoPaquete/Carta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrucoPaquete;
+
+
+public class Carta {
+    private int valor;
+    private String pinta;
+    
+    public Carta(int valor,String pinta) {
+       this.valor=valor;
+       this.pinta=pinta;
+     
+    }
+  
+   
+}
